5f3880098663a98a7b9bee982f415a97f4758de6
SalmanHersi <hersi.salman@gmail.com>
2023-10-03 15:34:47 -0400

    add jsx structure to navbar

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <div>
+        <h2>Logo Here</h2>
+      </div>
+      <div>Profile</div>
+    </div>
+  );
+};
+
+export default navbar;
