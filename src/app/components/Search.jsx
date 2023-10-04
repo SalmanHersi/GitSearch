83f6ddab101538a395797b18b63101032a19a22a
SalmanHersi <hersi.salman@gmail.com>
2023-10-04 07:58:27 -0400

    complete search bar

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -1,6 +1,9 @@
+"use client";
 import React from "react";
+import { useState } from "react";
 
 const Search = () => {
+  const [query, setQuery] = useState("");
   return (
     <form
       action=""
@@ -11,8 +14,15 @@ const Search = () => {
         placeholder="Type a username"
         className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 placeholder:text-center
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
-      <button type="submit" className=" bg-green-200 rounded-md px-4 py-1">
+      <button
+        type="submit"
+        className=" bg-green-200 rounded-md px-4 py-1"
+        disabled={!query}
+        style={{ opacity: !query ? 0.5 : 1 }}
+      >
         Search
       </button>
     </form>
