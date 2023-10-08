64c784830a76bb3242541c7fa23caedaf092783e
SalmanHersi <hersi.salman@gmail.com>
2023-10-07 21:07:51 -0400

    add animation when searching

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -87,7 +87,7 @@ const Search = ({ setUserData, setLoading }) => {
             disabled={!query || isLoading}
             style={{ opacity: !query || isLoading ? 0.5 : 1 }}
           >
-            Search
+            {isLoading ? "Searching..." : "Search"}
           </button>
           <button
             type="button"
@@ -100,10 +100,6 @@ const Search = ({ setUserData, setLoading }) => {
           </button>
         </div>
       </form>
-
-      {isLoading && (
-        <div className="text-gray-500 px-4 py-2 mt-2">Loading...</div>
-      )}
 
       {error && (
         <div className="bg-red-100 text-red-700 px-4 py-2 rounded-md mt-2">
