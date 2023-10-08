d60e5322beedbd7544264a05d70e0ddd29c5a9d7
SalmanHersi <hersi.salman@gmail.com>
2023-10-07 21:01:21 -0400

    add auto suggest function

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -1,39 +1,38 @@
-"use client";
-import React, { useState } from "react";
-import { Button, Input, useToast } from "@/app/chakra";
+import React, { useState, useEffect } from "react";
 
 const Search = ({ setUserData, setLoading }) => {
   const [query, setQuery] = useState("");
   const [error, setError] = useState("");
-  const toast = useToast();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!query) return;
-    setLoading(true); //Loading function
-    setUserData(null); //No initial data
-    try {
-      const res = await fetch(`https://api.github.com/users/${query}`);
-      const data = await res.json();
-      console.log("Data test", data);
-      // Check if the user exists
-      if (data.message) {
-        setError("User not found");
-        return; // Exit
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      if (query) {
+        performSearch(query);
       }
-      setUserData(data); // If no error, fetch data
-      addToLocalStorage(data, query);
+    }, 900);
+
+    return () => clearTimeout(timerId);
+  }, [query]);
+
+  const performSearch = async (searchTerm) => {
+    setLoading(true);
+    setIsLoading(true);
+    setUserData(null);
+    try {
+      const res = await fetch(`https://api.github.com/users/${searchTerm}`);
+      const data = await res.json();
+      if (!res.ok || data.message) {
+        setError("User not found");
+        return;
+      }
+      setUserData(data);
+      addToLocalStorage(data, searchTerm);
     } catch (error) {
-      toast({
-        title: "Error",
-        description: error.message,
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-      });
-      // setError(error.message);
+      setError(error.message);
     } finally {
-      setLoading(false); //Remove spinner
+      setLoading(false);
+      setIsLoading(false);
     }
   };
 
@@ -53,30 +52,58 @@ const Search = ({ setUserData, setLoading }) => {
 
     localStorage.setItem("github-users", JSON.stringify(users));
   };
+
+  const clearSearch = () => {
+    setQuery("");
+    setUserData(null);
+    setError("");
+    setLoading(false);
+    setIsLoading(false);
+  };
+
   return (
     <div>
       <form
-        onSubmit={handleSubmit}
-        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          performSearch(query);
+        }}
         className="flex flex-col p-4 justify-center items-center space-y-5 mt-3 text-center"
       >
         <input
           type="text"
           placeholder="Type a username"
-          className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 placeholder:text-center
-          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+          className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 placeholder:text-center focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
           value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          onChange={(e) => {
+            setError("");
+            setQuery(e.target.value);
+          }}
         />
-        <button
-          type="submit"
-          className="bg-green-200 rounded-md px-4 py-1"
-          disabled={!query}
-          style={{ opacity: !query ? 0.5 : 1 }}
-        >
-          Search
-        </button>
+        <div className="flex space-x-3">
+          <button
+            type="submit"
+            className="bg-green-200 rounded-md px-4 py-1"
+            disabled={!query || isLoading}
+            style={{ opacity: !query || isLoading ? 0.5 : 1 }}
+          >
+            Search
+          </button>
+          <button
+            type="button"
+            className="bg-red-200 rounded-md px-4 py-1"
+            onClick={clearSearch}
+            disabled={isLoading}
+            style={{ opacity: isLoading ? 0.5 : 1 }}
+          >
+            Clear Search
+          </button>
+        </div>
       </form>
+
+      {isLoading && (
+        <div className="text-gray-500 px-4 py-2 mt-2">Loading...</div>
+      )}
 
       {error && (
         <div className="bg-red-100 text-red-700 px-4 py-2 rounded-md mt-2">
